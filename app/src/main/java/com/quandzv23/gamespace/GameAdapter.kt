1d73df51830b0d02acad2e75d7aed941ecfb2fe7
quandzvl12 <136832812+quandzv23@users.noreply.github.com>
2026-07-27 20:05:59 +0700

    Update GameAdapter.kt

--- app/src/main/java/com/quandzv23/gamespace/GameAdapter.kt
+++ app/src/main/java/com/quandzv23/gamespace/GameAdapter.kt
@@ -54,15 +54,14 @@ class GameAdapter(
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         val pkg = packages[position]
-        val label: String
-        val icon: android.graphics.drawable.Drawable?
+        var label: String = pkg
+        var icon: android.graphics.drawable.Drawable? = null
         try {
             val appInfo = packageManager.getApplicationInfo(pkg, 0)
             icon = packageManager.getApplicationIcon(appInfo)
             label = packageManager.getApplicationLabel(appInfo).toString()
         } catch (e: PackageManager.NameNotFoundException) {
-            icon = null
-            label = pkg
+           
         }
 
         val launchAction = {
